0a8abaa966b26477cb4bae7ea52279cb50bcce1a
Ryan Burton <rryanburton@gmail.com>
2015-10-21 16:41:19 -0400

    2nd commit

--- todomvc/todo/views.py
+++ todomvc/todo/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import ToDo
-from .serializers import ContactSerializer
+from .serializers import ToDoSerializer
 
 # Create your views here.
+
+
+class ToDoViewSet(viewsets.ModelViewSet):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
