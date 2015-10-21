ee09a04cb29c80e50e7855d721a0006fa2892481
Ryan Burton <rryanburton@gmail.com>
2015-10-21 14:54:12 -0400

    intial commit

--- todomvc/todo/models.py
+++ todomvc/todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ToDo:
+    title = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
+    order = models.IntegerField(blank=True, null=True)
--- todomvc/todo/views.py
+++ todomvc/todo/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import ToDo
+from .serializers import ContactSerializer
 
 # Create your views here.
